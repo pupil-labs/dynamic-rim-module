96b36d03e24e15201310c128bc9c8ab2dfd4979f
Pablo Prietz <pablo@prietz.org>
2022-09-13 14:18:01 +0200

    Fix version parsing

--- src/pupil_labs/dynamic_content_on_rim/__init__.py
+++ src/pupil_labs/dynamic_content_on_rim/__init__.py
@@ -3,7 +3,7 @@
 from importlib.metadata import PackageNotFoundError, version
 
 try:
-    __version__ = version("pupil_labs.dynamic_content_on_rim")
+    __version__ = version("pupil-labs-dynamic-rim")
 except PackageNotFoundError:
     # package is not installed
     pass

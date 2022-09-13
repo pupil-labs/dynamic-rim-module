0385b795a32491d95fd8ca9acb7719ac4136d478
Pablo Prietz <pablo@prietz.org>
2022-09-13 14:01:36 +0200

    No need to install importlib with a min py requirment of 3.8

--- src/pupil_labs/dynamic_content_on_rim/__init__.py
+++ src/pupil_labs/dynamic_content_on_rim/__init__.py
@@ -1,9 +1,6 @@
 """Top-level entry-point for the <dynamic_content_on_rim> package"""
 
-try:
-    from importlib.metadata import PackageNotFoundError, version
-except ImportError:
-    from importlib_metadata import PackageNotFoundError, version
+from importlib.metadata import PackageNotFoundError, version
 
 try:
     __version__ = version("pupil_labs.dynamic_content_on_rim")
